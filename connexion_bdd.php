74a6244b676b00eb016faa80b93d9e1d0a30d73d
kenza <kenzaattia6@gmail.com>
2026-05-18 18:34:20 +0200

    connexion bdd

--- connexion_bdd.php
+++ connexion_bdd.php
@@ -2,34 +2,18 @@
 
 $serveur = "fdb1031.your-hosting.net";
 $utilisateur = "4760301_ng2groupe3h26";
-$motdepasse = "262026henrilekk+1@246";
+$motdepasse_bdd = "262026henrilekk+1@246";
 $base = "4760301_ng2groupe3h26";
 
-$connexion = mysqli_connect(
-    $serveur,
-    $utilisateur,
-    $motdepasse,
-    $base
-);
+$connexion = mysqli_connect($serveur, $utilisateur, $motdepasse_bdd, $base);
 
 if (!$connexion) {
-    die("Erreur de connexion");
+    die("Erreur de connexion : " . mysqli_connect_error());
 }
 
-try
-{
-$host = 'fdb1031 .your - hosting . net ';
-$nom_base = '4470413 _test ';
-$utilisateur = '4470413 _test ';
-$mot_de_passe = 'votre mot de passe ... ';
-$bdd = new PDO ('mysql : host =' . $host . '; dbname ='. $nom_base .';
-charset = utf8 ', $utilisateur , $mot_de_passe ,
-array (PDO :: ATTR_ERRMODE => PDO :: ERRMODE_EXCEPTION ));
-}
-catch ( Exception $e)
-{
-die ('Erreur : ' . $e - > getMessage () ) ;
-}
+mysqli_set_charset($connexion, "utf8");
 
 ?>
+
+
 
