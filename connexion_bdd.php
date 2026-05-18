09e5decd8e4d4b2da025d8a5e90c91bded288e01
kenza <kenzaattia6@gmail.com>
2026-05-18 17:23:35 +0200

    connexion bdd changé pour accordé a la base de donnée

--- connexion_bdd.php
+++ connexion_bdd.php
@@ -16,4 +16,20 @@ if (!$connexion) {
     die("Erreur de connexion");
 }
 
+try
+{
+$host = 'fdb1031 .your - hosting . net ';
+$nom_base = '4470413 _test ';
+$utilisateur = '4470413 _test ';
+$mot_de_passe = 'votre mot de passe ... ';
+$bdd = new PDO ('mysql : host =' . $host . '; dbname ='. $nom_base .';
+charset = utf8 ', $utilisateur , $mot_de_passe ,
+array (PDO :: ATTR_ERRMODE => PDO :: ERRMODE_EXCEPTION ));
+}
+catch ( Exception $e)
+{
+die ('Erreur : ' . $e - > getMessage () ) ;
+}
+
 ?>
+
